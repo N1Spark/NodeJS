59ddce8d684ab1839b372e81906ae254aa811901
Sarp_dg92@student.itstep.org <Sarp_dg92@student.itstep.org>
2024-09-12 22:52:57 +0300

    Comin

--- 6.09/index.js
+++ 6.09/index.js
@@ -1,21 +1,28 @@
+// import "dotenv/config";
+// // import dotenv from "dotenv";
+// // dotenv.config();
+// console.log(process.env.APP_TITLE);
+
 // const interval = setInterval(() => {
-//     console.log("sI")
+//     console.log("sI");
 // }, 1000);
 
-// setTimeout(() => {
-//     clearInterval(interval)
-// }, 4000)
+// setTimeout(() =>{
+//     clearInterval(interval);
+// }, 4000);
 
-// setImmediate(()=>{
-//     console.log("set imadiate");
+// setImmediate(() => {
+//     console.log("set Immediate")
 // });
 
-// Promise.resolve().then(()=>{
-//     console.log("Promise");
+// queueMicrotask(() => {
+//     console.log("microtask")
 // });
-// queueMicrotask(()=>{
-//     console.log("micritask");
+
+// Promise.resolve().then(() => {
+//    console.log("Promise"); 
 // });
+
 
 // const product = {
 //     id: 1,
@@ -23,13 +30,14 @@
 //     price: 30000,
 // };
 
-// const product2 = {...product}; //глибока копiя
+// const product2 = structuredClone(product);  // node глубокая копия
 // product.id = 2;
 // console.table(product2);
 
-// const product2 = product; //поверхностное копирование
+// const product2 = {...product};      //глубокая копия
 // product.id = 2;
 // console.table(product2);
+
 
 // const user = {
 //     id: 1,
@@ -40,78 +48,98 @@
 //     },
 // };
 
+// // const user2 = {...user}; // вложенные объекты не будут глубоко копироваться
 // const user2 = structuredClone(user);
 // user.address.ap = 100;
-// console.log(user);
-// const user2 = {...user};
-// user.id = 100;
 // console.table(user2);
+// console.log();
+// console.log(user);
+// console.log(user2);
 
 // const str = "Hello";
 // const encoding = btoa(str); //base64 кодирование
 // console.log(encoding);
 // const decoding = atob(encoding);
 // console.log(decoding);
+
 // const start = performance.now();
 // let sum = 0;
 // for (let i = 0; i < 1e7; i++) {
 //     sum += 1;
 // }
 // console.log(sum);
-// console.log(`Time: ${performance.now() - start}`);
+// console.log(`Time : ${performance.now() - start}`); 
 
-class DBService {
-    Get() {
-        fetch('http://localhost:3000/products')
-            .then(data => data.json())
-            .then(res => console.table(res))
-            .catch();
+// fetch('http://localhost:3000/users', {
+//     method: "POST",
+//     body: JSON.stringify({
+//         name: "Nikita",
+//     }),
+// })
+// .then((data) => console.log(data))
+// .catch();
+
+const serverProductAPI = "http://localhost:3000/products"
+
+class DbService {
+    Get(name){
+        if(name != null)
+            fetch(`${serverProductAPI}?product_name=${name}`).then(data => data.json()).then(res => console.table(res)).catch();
+        else
+            fetch(serverProductAPI).then(data => data.json()).then(res => console.table(res)).catch();
     }
-    Post(Name, Price, Description) {
-        fetch('http://localhost:3000/products', {
+    Post(){
+        fetch(serverProductAPI, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({
-                name: Name,
-                price: Price,
-                description: Description
+                product_name: "Office chair",
+                price: 19.99,
+                stock_quantity: 30,
+                description: "A chair that naturally adapts to the sitter, for people of different weights and heights."
             })
         })
-            .then(data => data.json())
-            .then(res => console.table(res))
-            .catch();
+        .then(data => data.json())
+        .then(res => console.table(res))
+        .catch();
     }
-    Put(id, Name, Price, Description) {
-        fetch(`http://localhost:3000/products/${id}`, {
+    Put(id){
+        fetch(`${serverProductAPI}/${id}`, {
             method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({
-                name: Name,
-                price: Price,
-                description: Description
+                product_name: "Office chair",
+                price: 39.99,
+                stock_quantity: 10,
+                description: "A chair that naturally adapts to the sitter, for people of different weights and heights."
             })
         })
-            .then(data => data.json())
-            .then(res => console.table(res))
-            .catch();
+        .then(data => data.json())
+        .then(res => console.table(res))
+        .catch();
     }
-    Delete(id) {
-        fetch(`http://localhost:3000/products/${id}`, {
+    Delete(id){
+        fetch(`${serverProductAPI}/${id}`, {
             method: 'DELETE',
         })
-            .then(data => {
-                if (data.ok) {
-                    console.log("Deleted")
-                }
-                else {
-                    console.log("Not Deleted")
-                }
-            }).catch();
-
+        .then(data => {
+            if(data.ok)
+                console.log("Product successfully deleted")
+            else
+                console.log("Delete failed")
+        })
+        .then(res => console.table(res))
+        .catch();
     }
+
 }
-const services = new DBService();
+const services = new DbService();
 services.Get();
-// services.Post("Test", 100, "Test descr");
-// services.Put("96f2", "PutTest", 100, "PutTest descr");
-services.Delete("6dea")
+//services.Get("Gaming Mouse");
+//services.Post();
+//services.Put("5f81");
+//services.Delete("433d");
